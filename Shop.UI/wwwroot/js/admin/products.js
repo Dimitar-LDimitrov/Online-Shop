ab3232360236842922d9becb6659b99abe682438
Dimitrov <dimitrar-ldimitrovv@gmail.com>
2021-04-02 15:45:51 +0300

    Create custom Layout

--- Shop.UI/wwwroot/js/admin/products.js
+++ Shop.UI/wwwroot/js/admin/products.js
@@ -1,60 +1,16 @@
-﻿Vue.component('product-manager', {
-    template: `<div>
-                <div v-if="!editing">
-                    <button class="button" @click="newProduct">Add New Product</button>
-                    <table class="table">
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Value</th>
-                        </tr>
-                        <tr v-for="(product, index) in products">
-                            <td>{{product.id}}</td>
-                            <td>{{product.name}}</td>
-                            <td>{{product.value}}</td>
-                            <td><button @click="editProduct(product.id, index)">Edit</button></td>
-                            <td><button v-on:click="deleteProduct(product.id, index)">Remove</button></td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div v-else>
-                    <div class="field">
-                        <div class="label">Product Name</div>
-                        <div class="control">
-                            <input class="input" v-model="productModel.name" />
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="label">Product Description</div>
-                        <div class="control">
-                            <input class="input" v-model="productModel.description" />
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="label">Product Value</div>
-                        <div class="control">
-                            <input class="input" v-model="productModel.value" />
-                        </div>
-                    </div>
-                    <button class="button is-success" v-on:click="createProduct" v-if="!productModel.id">Create Product</button>
-                    <button class="button is-warning" v-on:click="updateProduct" v-else>Update Product</button>
-                    <button class="button" v-on:click="cancel">Cancel</button>
-                </div>
-            </div>`,
-    data() {
-        return {
-            editing: false,
-            loading: false,
-            objectIndex: 0,
-            productModel: {
-                id: 0,
-                name: "Name",
-                description: "Description",
-                value: 1.99
-            },
-            products: [],
-        }
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        editing: false,
+        loading: false,
+        objectIndex: 0,
+        productModel: {
+            id: 0,
+            name: "Name",
+            description: "Description",
+            value: 1.99
+        },
+        products: [],
     },
     mounted() {
         this.getProducts();
